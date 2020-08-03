3f1087fb34bb81d1596007c5d58f6eddd705e962
f.estacio@gmail.com <f.estacio@gmail.com>
2020-08-03 08:40:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './dwc-logo-flat.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="logo" alt="Digital Water Consulting logo" />
+        <button className="menu-button">Menu</button>
       </header>
     </div>
   );
